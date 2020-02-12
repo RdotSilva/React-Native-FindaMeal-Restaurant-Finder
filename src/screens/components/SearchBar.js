2a24edf3f4b2c3f07c4501125ef79c84568ada17
RdotSilva <RyandotSilva@gmail.com>
2020-02-11 19:41:44 -0500

    Add text input for search

--- src/screens/components/SearchBar.js
+++ src/screens/components/SearchBar.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 const SearchBar = () => {
   return (
     <View style={styles.background}>
       <MaterialIcons name="search" size={30} />
-      <Text>Search Bar</Text>
+      <TextInput></TextInput>
     </View>
   );
 };
@@ -16,7 +16,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#F0EEEE",
     height: 50,
     borderRadius: 5,
-    marginHorizontal: 15
+    marginHorizontal: 15,
+    flexDirection: "row"
   }
 });
 
