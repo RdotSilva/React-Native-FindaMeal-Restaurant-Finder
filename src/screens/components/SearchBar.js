741c5b1ac223f95e5f178812b505a9d585b878b3
RdotSilva <RyandotSilva@gmail.com>
2020-02-11 19:39:24 -0500

    Add search icon

--- src/screens/components/SearchBar.js
+++ src/screens/components/SearchBar.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const SearchBar = () => {
   return (
-    <View>
+    <View style={styles.background}>
+      <MaterialIcons name="search" size={30} />
       <Text>Search Bar</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15
+  }
+});
 
 export default SearchBar;
