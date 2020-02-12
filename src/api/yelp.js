810b19e671eeab1a41e8375c4b3866c2fe3c404c
RdotSilva <RyandotSilva@gmail.com>
2020-02-12 09:15:08 -0500

    Update yelp api helper

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { YELP_API_KEY } from "react-native-dotenv";
+import keys from "../../config/config.js";
 
 export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
-    Authorization: `Bearer ${YELP_API_KEY}`
+    Authorization: `Bearer ${keys.yelpApiKey}`
   }
 });
