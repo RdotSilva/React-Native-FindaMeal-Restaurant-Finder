20f32b8deffa942215ecf64c08e6ffee957dd493
RdotSilva <RyandotSilva@gmail.com>
2020-02-11 19:52:51 -0500

    Update searchbar text input properties

--- src/screens/components/SearchBar.js
+++ src/screens/components/SearchBar.js
@@ -2,11 +2,18 @@ import React from "react";
 import { View, TextInput, StyleSheet } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
   return (
     <View style={styles.backgroundStyle}>
       <MaterialIcons name="search" style={styles.iconStyle} />
-      <TextInput style={styles.inputStyle} placeholder={"Search"}></TextInput>
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder={"Search"}
+        value={term}
+        onChangeText={newTerm => onTermChange(newTerm)}
+      ></TextInput>
     </View>
   );
 };
