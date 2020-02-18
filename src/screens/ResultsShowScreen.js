033f7ac42fcd66bf5f3a85451e026dcdbff99407
RdotSilva <RyandotSilva@gmail.com>
2020-02-17 21:09:13 -0500

    Add getResult method

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -5,6 +5,12 @@ import yelp from "../api/yelp";
 const ResultsShowScreen = ({ navigation, route }) => {
   const [result, setResult] = useState(null);
   const id = route.params.id;
+
+  const getResult = async id => {
+    const res = await yelp.get(`/${id}`);
+    setResult(res.data);
+  };
+
   return (
     <View>
       <Text></Text>
