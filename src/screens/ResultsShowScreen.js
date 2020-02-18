93c9569a2e420a9bd52792f39731f610cb30c031
RdotSilva <RyandotSilva@gmail.com>
2020-02-17 21:08:29 -0500

    Add result state

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import yelp from "../api/yelp";
 
 const ResultsShowScreen = ({ navigation, route }) => {
+  const [result, setResult] = useState(null);
   const id = route.params.id;
   return (
     <View>
