7bb92531316e86deb3192bd12ed54a43c0dcf79a
RdotSilva <RyandotSilva@gmail.com>
2020-02-19 12:15:53 -0500

    Add ImageCollection component

---
+++ src/components/ImageCollection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, FlatList } from "react-native";
+
+const ImageCollection = () => {
+  return (
+    <View>
+      <Text>Image Collection</Text>
+    </View>
+  );
+};
+
+export default ImageCollection;
