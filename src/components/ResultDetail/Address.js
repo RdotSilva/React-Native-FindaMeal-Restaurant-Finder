5d8f0efc23010f4d0fb6a1f91990891b3cc52f78
RdotSilva <RyandotSilva@gmail.com>
2020-02-20 12:44:22 -0500

    Add builtAddress from results

--- src/components/ResultDetail/Address.js
+++ src/components/ResultDetail/Address.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 const Address = ({ result }) => {
+  const builtAddress = `${result.location.address1} ${result.location.city}, ${result.location.state}`;
   return (
     <View>
       <Text>
