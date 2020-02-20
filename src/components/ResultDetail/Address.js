8b0b1e626c63185836ed7a7b4621061763c0bc45
RdotSilva <RyandotSilva@gmail.com>
2020-02-20 12:32:14 -0500

    Add Address component

---
+++ src/components/ResultDetail/Address.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Address = () => {
+  return (
+    <View>
+      <Text>Address</Text>
+    </View>
+  );
+};
+
+export default Address;
