ecd2ceeaf2c3f486104cdfc913f4f12c5477483b
RdotSilva <RyandotSilva@gmail.com>
2020-02-20 12:12:43 -0500

    Add openStyles

--- src/components/ResultDetail/OpenOrClosed.js
+++ src/components/ResultDetail/OpenOrClosed.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const OpenOrClosed = ({ result }) => {
   return <Text>{result.is_closed ? "CLOSED" : "OPEN"}</Text>;
 };
+
+const styles = StyleSheet.create({
+  openStyles: {
+    color: "#83B692"
+  }
+});
 
 export default OpenOrClosed;
